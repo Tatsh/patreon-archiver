ed19ec2e54ebc1ddad003e8fbfa8375db4b8b993
Andrew Udvare <audvare@gmail.com>
2022-01-25 10:21:45 -0500

    patreon_archiver/constants: switch to posts URI

--- patreon_archiver/constants.py
+++ patreon_archiver/constants.py
@@ -1,11 +1,10 @@
 from typing import Final, Mapping
 
-__all__ = ('PATREON_API_URI', 'STREAM_URI', 'MEDIA_URI', 'FIELDS',
-           'USER_AGENT')
+__all__ = ('FIELDS', 'MEDIA_URI', 'PATREON_API_URI', 'POSTS_URI', 'USER_AGENT')
 
 PATREON_API_URI: Final[str] = 'https://www.patreon.com/api'
-STREAM_URI: Final[str] = f'{PATREON_API_URI}/stream'
 MEDIA_URI: Final[str] = f'{PATREON_API_URI}/media'
+POSTS_URI: Final[str] = f'{PATREON_API_URI}/posts'
 FIELDS: Final[Mapping[str, str]] = dict(
     campaign=','.join(
         ('currency', 'show_audio_post_download_links', 'avatar_photo_url',
@@ -17,7 +16,7 @@ FIELDS: Final[Mapping[str, str]] = dict(
                    'min_cents_pledged_to_view', 'post_file', 'post_metadata',
                    'published_at', 'patreon_url', 'post_type', 'pledge_url',
                    'thumbnail_url', 'teaser_text', 'title', 'upgrade_url',
-                   'url', 'was_posted_by_campaign_owner')),
+                   'url', 'was_posted_by_campaign_owner', 'has_ti_violation')),
     post_tag=','.join(('tag_type', 'value')),
     user=','.join(('image_url', 'full_name', 'url')),
     access_rule=','.join(('access_rule_type', 'amount_cents')),
@@ -30,19 +29,21 @@ USER_AGENT: Final[str] = ('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
                           'Safari/537.36')
 SHARED_PARAMS: Final[Mapping[str, str]] = {
     **dict(include=','.join((
-        'user',
-        'attachments',
-        'user_defined_tags',
         'campaign',
+        'access_rules',
+        'attachments',
+        'audio',
+        'images',
+        'media',
         'poll.choices',
         'poll.current_user_responses.user',
         'poll.current_user_responses.choice',
         'poll.current_user_responses.poll',
-        'access_rules.tier.null',
-        'images.null',
-        'audio.null',
+        'user',
+        'user_defined_tags',
+        'ti_checks',
     )),
-           sort='published_at'),
+           sort='-published_at'),
     **{
         'filter[is_following]': 'true',
         'page[cursor]': 'null',
