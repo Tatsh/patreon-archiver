3b3c08e23928ee7ff9cf42a229aec02292ffd607
Andrew Udvare <audvare@gmail.com>
2022-01-26 19:13:34 -0500

    setup: fix license

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('README.md') as f:
           install_requires=[
               'click>=8.0.0', 'loguru>=0.5.3', 'requests', 'yt-dlp'
           ],
-          license='LICENSE.txt',
+          license='MIT',
           long_description=f.read(),
           long_description_content_type='text/markdown',
           name='patreon-archiver',
