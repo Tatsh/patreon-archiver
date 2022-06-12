84451c59e63ad69fe15af66e59841ac7329f29a0
Andrew Udvare <audvare@gmail.com>
2022-06-11 23:34:03 -0400

    setup: add pylint-quotes

--- setup.py
+++ setup.py
@@ -9,19 +9,18 @@ with open('README.md') as f:
           },
           extras_require={
               'dev': [
-                  'mypy', 'mypy-extensions', 'pylint', 'rope',
+                  'mypy', 'mypy-extensions', 'pylint', 'pylint-quotes', 'rope',
                   'types-requests>=2.25.9'
               ]
           },
           install_requires=[
-              'click>=8.0.0', 'loguru>=0.5.3',
-              'requests', 'yt-dlp'
+              'click>=8.0.0', 'loguru>=0.5.3', 'requests', 'yt-dlp'
           ],
           license='MIT',
           long_description=f.read(),
           long_description_content_type='text/markdown',
           name='patreon-archiver',
           packages=find_packages(),
-          python_requires=">=3.9",
+          python_requires='>=3.9',
           url='https://github.com/Tatsh/patreon-archiver',
           version='0.0.6')
